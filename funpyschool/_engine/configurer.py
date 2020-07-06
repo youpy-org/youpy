7a908537a188776cb6a3fffd561da342dffa782a
Nicolas Despres <nicolas.despres@gmail.com>
2020-07-06 16:37:06 +0200

    Move configuration stuff to configurer.py

---
+++ funpyschool/_engine/configurer.py
@@ -0,0 +1,74 @@
+# -*- encoding: utf-8 -*-
+"""
+"""
+
+
+import json
+
+from .error import FunPySchoolException
+from . import coordsys
+from .data import scale_sprite_by
+
+
+class ConfigError(FunPySchoolException):
+    pass
+
+class Configurer:
+
+    def __init__(self, engine):
+        self.engine = engine
+
+    def load(self, filepath):
+        try:
+            with open(filepath) as stream:
+                return json.load(stream)
+        except FileNotFoundError:
+            return {}
+
+    def configure(self):
+        cfg = self.load(self.engine.project.config_file)
+        self._configure_coordsys(self.engine, cfg)
+        self._configure_scene(self.engine.scene, cfg.get("scene"))
+        self._configure_sprites(self.engine.sprites, cfg.get("sprites"))
+
+    def _configure_coordsys(self, engine, cfg):
+        coordsys_name = cfg.get("coordinate_system", coordsys.DEFAULT)
+        try:
+            coordsys_class = getattr(coordsys, coordsys_name)
+        except AttributeError:
+            raise ConfigError(f"invalid coordinate system: '{coordsys_name}'")
+        else:
+            engine.coordsys = coordsys_class(engine.scene.center)
+
+    def _configure_scene(self, scene, cfg):
+        if cfg is None:
+            return
+        initial_backdrop = cfg.get("initial_backdrop")
+        if initial_backdrop is not None:
+            try:
+                scene.backdrop = scene.backdrops[initial_backdrop]
+            except KeyError:
+                raise ConfigError(
+                    f"invalid initial backdrop: '{initial_backdrop}'")
+
+    def _configure_sprites(self, sprites, cfg):
+        if cfg is None:
+            return
+        for sprite_name, sprite_cfg in cfg.items():
+            try:
+                sprite = sprites[sprite_name]
+            except KeyError:
+                raise ConfigError(f"unknown sprite: '{sprite_name}'")
+            else:
+                self._configure_sprite(sprite, sprite_cfg)
+
+    def _configure_sprite(self, sprite, cfg):
+        ratio = cfg.get("ratio")
+        scale_sprite_by(sprite, ratio=ratio)
+        position = cfg.get("position")
+        if position is not None:
+            self.engine.coordsys.rect_go_to(sprite.rect,
+                                            position["x"], position["y"])
+        visible = cfg.get("visible", True)
+        if visible:
+            sprite.visible = visible
--- funpyschool/_engine/engine.py
+++ funpyschool/_engine/engine.py
@@ -3,17 +3,14 @@
 """
 
 
-import json
-
 import pygame
 
 from funpyschool._project import Project
-from funpyschool._engine import coordsys
 from funpyschool._engine.tools import FrequencyMeter
 from funpyschool._engine.data import Color
-from funpyschool._engine.data import scale_sprite_by
 from funpyschool._engine.events import EventSet
 from funpyschool._engine.loader import Loader
+from funpyschool._engine.configurer import Configurer
 
 
 class Scene:
@@ -47,72 +44,6 @@ class ConsoleProgress:
 
     def end_section(self):
         print() # flush
-
-class FunPySchoolException(Exception):
-    pass
-
-class ConfigError(FunPySchoolException):
-    pass
-
-class Configurer:
-
-    def __init__(self, engine):
-        self.engine = engine
-
-    def load(self, filepath):
-        try:
-            with open(filepath) as stream:
-                return json.load(stream)
-        except FileNotFoundError:
-            return {}
-
-    def configure(self):
-        cfg = self.load(self.engine.project.config_file)
-        self._configure_coordsys(self.engine, cfg)
-        self._configure_scene(self.engine.scene, cfg.get("scene"))
-        self._configure_sprites(self.engine.sprites, cfg.get("sprites"))
-
-    def _configure_coordsys(self, engine, cfg):
-        coordsys_name = cfg.get("coordinate_system", coordsys.DEFAULT)
-        try:
-            coordsys_class = getattr(coordsys, coordsys_name)
-        except AttributeError:
-            raise ConfigError(f"invalid coordinate system: '{coordsys_name}'")
-        else:
-            engine.coordsys = coordsys_class(engine.scene.center)
-
-    def _configure_scene(self, scene, cfg):
-        if cfg is None:
-            return
-        initial_backdrop = cfg.get("initial_backdrop")
-        if initial_backdrop is not None:
-            try:
-                scene.backdrop = scene.backdrops[initial_backdrop]
-            except KeyError:
-                raise ConfigError(
-                    f"invalid initial backdrop: '{initial_backdrop}'")
-
-    def _configure_sprites(self, sprites, cfg):
-        if cfg is None:
-            return
-        for sprite_name, sprite_cfg in cfg.items():
-            try:
-                sprite = sprites[sprite_name]
-            except KeyError:
-                raise ConfigError(f"unknown sprite: '{sprite_name}'")
-            else:
-                self._configure_sprite(sprite, sprite_cfg)
-
-    def _configure_sprite(self, sprite, cfg):
-        ratio = cfg.get("ratio")
-        scale_sprite_by(sprite, ratio=ratio)
-        position = cfg.get("position")
-        if position is not None:
-            self.engine.coordsys.rect_go_to(sprite.rect,
-                                            position["x"], position["y"])
-        visible = cfg.get("visible", True)
-        if visible:
-            sprite.visible = visible
 
 class Renderer:
 
