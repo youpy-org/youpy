b0b5167334d2ae35f5bc548eeae81aad2f9edcab
Nicolas Despres <nicolas.despres@gmail.com>
2020-07-08 17:54:30 +0200

    Add looks.scene.

--- youpy/code/english/looks.py
+++ youpy/code/english/looks.py
@@ -3,9 +3,30 @@
 """
 
 
+from youpy._engine import get_running_engine
 from youpy._engine import send_request
 from youpy._engine import message
 
+
+class Scene:
+    """User interface to the scene read-only attributes.
+
+    WARNING: Make sure everything is read-only since they are accessed
+             concurrently.
+    """
+
+    # Make sure we have no attribute otherwise a lock will be required.
+    __slots__ = ()
+
+    @property
+    def width(self):
+        return get_running_engine().scene.width
+
+    @property
+    def height(self):
+        return get_running_engine().scene.height
+
+scene = Scene()
 
 def switch_to(backdrop):
     # TODO(Nicolas Despres): Add wait keyword argument to mimic the
