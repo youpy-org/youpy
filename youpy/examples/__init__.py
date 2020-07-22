f28946ccd7fb16ba93d61a04af510a362699f172
Nicolas Despres <nicolas.despres@gmail.com>
2020-07-22 14:30:40 +0200

    Add "run_example".

--- youpy/examples/__init__.py
+++ youpy/examples/__init__.py
@@ -1,3 +1,20 @@
 # -*- encoding: utf-8 -*-
 """Youpy's examples.
 """
+
+
+import os
+
+from youpy.runner import run as _run
+
+
+_EXAMPLES_DIR = os.path.dirname(__file__)
+
+def run_example(example_name):
+    if not isinstance(example_name, str):
+        raise TypeError("example_name must be str, not {}"
+                        .format(type(example_name).__name__))
+    example_path = os.path.join(_EXAMPLES_DIR, example_name)
+    if not os.path.exists(example_path):
+        raise ValueError(f"invalid example {example_name}")
+    _run(example_path)
