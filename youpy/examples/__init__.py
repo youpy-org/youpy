8c7874a51a420e5ba0e56b6c62bde3711c450530
Nicolas Despres <nicolas.despres@gmail.com>
2020-07-22 14:34:50 +0200

    Add "list_example_names()"

--- youpy/examples/__init__.py
+++ youpy/examples/__init__.py
@@ -6,6 +6,7 @@
 import os
 
 from youpy.runner import run as _run
+from youpy._project import INTERNAL_DIR as _INTERNAL_DIR
 
 
 _EXAMPLES_DIR = os.path.dirname(__file__)
@@ -18,3 +19,12 @@ def run_example(example_name):
     if not os.path.exists(example_path):
         raise ValueError(f"invalid example {example_name}")
     _run(example_path)
+
+def list_example_names():
+    results = []
+    for name in os.listdir(_EXAMPLES_DIR):
+        example_path = os.path.join(_EXAMPLES_DIR, name)
+        if os.path.isdir(example_path) \
+           and os.path.isdir(os.path.join(example_path, _INTERNAL_DIR)):
+            results.append(name)
+    return results
