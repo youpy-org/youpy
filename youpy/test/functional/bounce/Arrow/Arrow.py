e7f764a4e1b4e66a6b925c915a43470e2cb3963c
Nicolas Despres <nicolas.despres@gmail.com>
2020-09-13 10:28:15 +0200

    Update bounce test to check physics refresh rate.

--- youpy/test/functional/bounce/Arrow/Arrow.py
+++ youpy/test/functional/bounce/Arrow/Arrow.py
@@ -1,6 +1,25 @@
 from youpy.code.english.everything import *
+from time import time
 
 def when_space_key_pressed():
     go_to(-230, 0)
-    for _ in range(200):
+
+    console.print("All durations should be roughly equal")
+
+    t0 = time()
+    for _ in range(50):
         move(2)
+    t1 = time()
+    console.print(t1 - t0)
+
+    for _ in range(50):
+        move(1)
+        move(1)
+    t2 = time()
+    console.print(t2 - t1)
+
+    for _ in range(50):
+        move(2)
+        bounce_if_on_edge()
+    t3 = time()
+    console.print(t3 - t2)
