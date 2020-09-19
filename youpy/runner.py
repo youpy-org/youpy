0a62de1e494833c37d4a8104d0210f6dd9b3bd77
Nicolas Despres <nicolas.despres@gmail.com>
2020-09-19 12:32:28 +0200

    Initialize logger before anything else.

--- youpy/runner.py
+++ youpy/runner.py
@@ -8,7 +8,8 @@ from youpy.engine import Engine
 from youpy.engine import Simulation
 from youpy.project import Project
 from youpy.project import get_project_dir
-from youpy.logging import init_logger
+from youpy import logging
+LOGGER = logging.getLogger(__name__)
 
 
 def run(path,
@@ -16,9 +17,11 @@ def run(path,
         log_level=None, syslog_level=None, log_context=False):
     project_dir = get_project_dir(path)
     project = Project(project_dir)
+    logging.init_logger(project, log_level=log_level, syslog_level=syslog_level,
+                        log_context=log_context)
+    LOGGER.info("=" * 60)
+    LOGGER.info(f"running {project.name}")
     simu = Simulation(project, show_fps=show_fps)
     engine = Engine(simu, target_fps=fps)
-    init_logger(project, log_level=log_level, syslog_level=syslog_level,
-                log_context=log_context)
     with extended_sys_path(project_dir.parent):
         return engine.run()
