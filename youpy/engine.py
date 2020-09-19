f6ced74d9c2ef24776fda6f8958a9596aff701c1
Nicolas Despres <nicolas.despres@gmail.com>
2020-09-19 10:12:26 +0200

    Move SharedVariableSet to 'shared_variables' module.

--- youpy/engine.py
+++ youpy/engine.py
@@ -3,7 +3,6 @@
 """
 
 
-from collections.abc import MutableMapping
 from collections import OrderedDict
 from collections import Counter
 from abc import ABC
@@ -30,6 +29,7 @@ from youpy.concurrency import EmptyQueue
 from youpy.keys import iter_keys
 from youpy.keys import check_key
 from youpy import math
+from youpy.shared_variables import SharedVariableSet
 
 
 from youpy import logging
@@ -359,68 +359,6 @@ class RequestProcessors:
         def _run_once(self):
             LOGGER.info(f"script {self.script.name} has stopped the program")
             self.simu.stop(reason=self.request.reason)
-
-class SharedVariable:
-
-    def __init__(self, shared_variable_set, value):
-        self._set = shared_variable_set
-        self._value = value
-        self._visible = False
-
-    def get(self):
-        return self._value
-
-    def hide(self):
-        if not self._visible:
-            return
-        self._visible = False
-        self._set_changed()
-
-    def show(self):
-        if self._visible:
-            return
-        self._visible = True
-        self._set_changed()
-
-    def __iadd__(self, other):
-        old_value = self._value
-        self._value += other
-        if old_value != self._value:
-            self._set_changed()
-        return self
-
-    def _set_changed(self):
-        self._set._changed = True
-
-class SharedVariableSet(MutableMapping):
-
-    def __init__(self):
-        self._d = OrderedDict()
-        self._changed = False
-
-    def __setitem__(self, name, value):
-        self._d[name] = SharedVariable(self, value)
-        self._changed = True
-
-    def __getitem__(self, name):
-        return self._d[name]
-
-    def __delitem__(self, name):
-        del self._d[name]
-        self._changed = True
-
-    def __iter__(self):
-        return iter(self._d)
-
-    def __len__(self):
-        return len(self._d)
-
-    @property
-    def has_changed(self):
-        return self._changed
-
-    def clear_change_flag(self):
-        self._changed = False
 
 class EventManager:
 
